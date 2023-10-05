6006b292174d1382b3d23dd234d50d98169c3901
Tamannaaa17 <110052631+Tamannaaa17@users.noreply.github.com>
2023-10-06 00:31:59 +0530

    Add files via upload

---
+++ components/SearchData.js
@@ -0,0 +1,44 @@
+const Data =[
+    {
+        name:'Made for you',
+        color:'#FE4773'
+    },
+    {
+        name:'Podcasts',
+        color:'#8F30A1'
+    },
+    {
+        name:'Jazz',
+        color:'#46B3A5'
+    },
+    {
+        name:'Chill',
+        color:'#2E6D92'
+    },
+    {
+        name:'Soul',
+        color:'#F6D68D'
+    },
+    {
+        name:'Lofi',
+        color:'#580C82'
+    },
+    {
+        name:'Metal',
+        color:'#46B3A5'
+    },
+    {
+        name:'Rock',
+        color:'#FE4773'
+    },
+    {
+        name:'Blues',
+        color:'#2E6D92'
+    },
+    {
+        name:'R&B',
+        color:'#F6D68D'
+    },
+];
+
+export default Data;
